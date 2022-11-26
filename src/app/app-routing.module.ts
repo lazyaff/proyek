d5aca687388ad3d96a462a678da6d86160c06c93
sfmuazam <sfmuazam@gmail.com>
2022-11-26 08:42:30 +0700

    modified:   angular.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	src/app/anggota/ 	src/app/anggota_edit/ 	src/app/anggota_tambah/ 	src/app/api.service.spec.ts 	src/app/api.service.ts 	src/app/buku/ 	src/app/buku_edit/ 	src/app/buku_tambah/ 	src/app/guards/ 	src/app/login/ 	src/app/peminjaman/ 	src/app/peminjaman_edit/ 	src/app/peminjaman_tambah/ 	src/app/services/

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,66 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AutoLoginGuard } from './guards/auto-login.guard';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'peminjaman',
+    loadChildren: () => import('./peminjaman/peminjaman.module').then(m => m.PeminjamanPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'peminjaman_tambah',
+    loadChildren: () => import('./peminjaman_tambah/peminjaman_tambah.module').then(m => m.PeminjamanTambahPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'peminjaman_edit/:id',
+    loadChildren: () => import('./peminjaman_edit/peminjaman_edit.module').then(m => m.PeminjamanEditPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'buku',
+    loadChildren: () => import('./buku/buku.module').then(m => m.BukuPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'buku_tambah',
+    loadChildren: () => import('./buku_tambah/buku_tambah.module').then(m => m.BukuTambahPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'buku_edit/:id',
+    loadChildren: () => import('./buku_edit/buku_edit.module').then(m => m.BukuEditPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'anggota',
+    loadChildren: () => import('./anggota/anggota.module').then(m => m.AnggotaPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'anggota_tambah',
+    loadChildren: () => import('./anggota_tambah/anggota_tambah.module').then(m => m.AnggotaTambahPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'anggota_edit/:id',
+    loadChildren: () => import('./anggota_edit/anggota_edit.module').then(m => m.AnggotaEditPageModule),
+    canLoad: [AuthGuard]
+  },
+
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule),
+    canLoad: [AutoLoginGuard]
   }
+
 ];
 
 @NgModule({
@@ -19,4 +69,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+{ title: 'Peminjaman', url: '/peminjaman', icon: 'cube' },
+{ title: 'Buku', url: '/buku', icon: 'cube' },
+{ title: 'Anggota', url: '/anggota', icon: 'cube' },
     { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
